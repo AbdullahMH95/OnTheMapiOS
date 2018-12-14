5eb8c310115d2f4a511e29755be7f6fbb077b8f6
AbdullahMH95 <mhr5115@gmail.com>
2018-12-14 21:04:46 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by Abdullah Al-Mahry on 14/12/2018.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
